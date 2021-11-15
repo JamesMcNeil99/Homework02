aa4ca197c3bb7fdc973127575dc8ca3f67db84c0
James McNeil <mcneiljt@jacks.sfasu.edu>
2021-11-15 11:54:10 -0600

    Add project files.

---
+++ Homework02/Genre.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Homework02
+{
+    /// <summary>
+    /// Interaction logic for Genre.xaml
+    /// </summary>
+    public partial class Genre : Window
+    {
+        public Genre()
+        {
+            InitializeComponent();
+            addGenresToList();
+        }
+        public void addGenresToList()
+        {
+            cmbGenre.Text = "Choose movie genre";
+            List<string> genres = new List<string>();
+            genres.Add("ACTION");
+            genres.Add("COMEDY");
+            genres.Add("ROMANCE");
+            genres.Add("DOCUMENTARY");
+            cmbGenre.ItemsSource = genres;
+        }
+    }
+}
